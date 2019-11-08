7440545e66b3c472e1c065aee3a8395891776f13
stscoundrel <sampo@pixels.fi>
2019-11-08 09:54:25 +0200

    Persons controller: add post / create route

--- controllers/persons.js
+++ controllers/persons.js
@@ -3,9 +3,33 @@ const personsRouter = require('express').Router()
 // Models
 const Person = require('../models/Person')
 
-personsRouter.get('/', async (request, response) => {
-  const people = await Person.query()
-  response.json(people)
+/**
+ * Return list of all persons
+ */
+personsRouter.get('/', async (request, response, next) => {
+  try {
+    const people = await Person.query()
+    response.json(people)
+  } catch (error) {
+    next(error)
+  }
+})
+
+/**
+ * Create new person.
+ */
+personsRouter.post('/', async (request, response, next) => {
+  console.log(request.body)
+
+  try {
+    const person = await Person
+      .query()
+      .insert(request.body)
+
+    response.status(201).json(person.toJSON())
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = personsRouter
--- models/Person.js
+++ models/Person.js
@@ -4,6 +4,19 @@ class Person extends Model {
   static get tableName() {
     return 'persons';
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['firstName', 'lastName', 'email'],
+      properties: {
+        id: {type: 'integer'},
+        firstName: {type: 'string', minLength: 1, maxLength: 50},
+        lastName: {type: 'string', minLength: 1, maxLength: 50},
+        email: {type: 'email'},
+      }
+    };
+  }
 }
 
 module.exports = Person;
